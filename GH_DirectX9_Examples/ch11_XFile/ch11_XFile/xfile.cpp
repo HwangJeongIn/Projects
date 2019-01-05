c3b1dbc6f0f1ba72a92982e98bc427dd77440789
HwangJeongIn <36975010+HwangJeongIn@users.noreply.github.com>
2019-01-05 21:56:04 +0900

    XFile예제
    2

--- GH_DirectX9_Examples/ch11_XFile/ch11_XFile/xfile.cpp
+++ GH_DirectX9_Examples/ch11_XFile/ch11_XFile/xfile.cpp
@@ -31,6 +31,7 @@ std::vector<IDirect3DTexture9*> Textures(0);
 //
 bool Setup()
 {
+	// 오류발생시 예외처리를 하기 위한 변수
 	HRESULT hr = 0;
 
 	//
@@ -41,14 +42,24 @@ bool Setup()
 	ID3DXBuffer* mtrlBuffer = 0;
 	DWORD        numMtrls   = 0;
 
-	hr = D3DXLoadMeshFromX(  
+	// X파일을 읽는다.
+	hr = D3DXLoadMeshFromX(
+		// 파일명
 		"bigship1.x",
+		//메쉬는 관리 메모리 풀내에 보관된다.
 		D3DXMESH_MANAGED,
+		// 메쉬와 연계될 장치
 		Device,
+		// 메쉬 접근 정보를 위한 DWORD배열을 포함하는 버퍼 리턴
 		&adjBuffer,
+		// 메쉬 재질 정보를 위한 버퍼 리턴
+		// 여기서 반환되는 재질정보와 서브셋과의 배열의 인덱스는 1대1로 대응된다.
 		&mtrlBuffer,
+
 		0,
+		// 재질의 수 리턴
 		&numMtrls,
+		// 메쉬 객체 리턴한다.
 		&Mesh);
 
 	if(FAILED(hr))
@@ -61,14 +72,21 @@ bool Setup()
 	// Extract the materials, and load textures.
 	//
 
+	// 재질을 추출하고 텍스처를 읽어들인다.
+	// 있을때만 처리해준다.
 	if( mtrlBuffer != 0 && numMtrls != 0 )
 	{
+		// 그냥 버퍼자체의 정보만을 가지고 있기 때문에 
+		// D3DXMATERAL 형식으로 읽으려면 타입캐스팅이 필요하다.
+		// 여기서는 버퍼의 내용을 확실히 알고 있기 때문에 바로 C타입 타입캐스팅을 해주었다.
 		D3DXMATERIAL* mtrls = (D3DXMATERIAL*)mtrlBuffer->GetBufferPointer();
 
 		for(int i = 0; i < numMtrls; i++)
 		{
 			// the MatD3D property doesn't have an ambient value set
 			// when its loaded, so set it now:
+
+			// 로드될때 ambient값을 가지지 않으므로 이를 지정한다.
 			mtrls[i].MatD3D.Ambient = mtrls[i].MatD3D.Diffuse;
 
 			// save the ith material
@@ -78,6 +96,7 @@ bool Setup()
 			if( mtrls[i].pTextureFilename != 0 )
 			{
 				// yes, load the texture for the ith subset
+				// 파일이름이 정상적으로 들어가 있을때만 그 파일명으로 파일을 열어서 텍스처를 로드한다.
 				IDirect3DTexture9* tex = 0;
 				D3DXCreateTextureFromFile(
 					Device,
@@ -85,28 +104,36 @@ bool Setup()
 					&tex);
 
 				// save the loaded texture
+				// 텍스처가 있을때 그 텍스처를 넣어주고
 				Textures.push_back( tex );
 			}
 			else
 			{
 				// no texture for the ith subset
+				// 없을때도 서브셋과 같은 인덱스 번호를 맞춰주기 위해서 널값을 넣어준다.
 				Textures.push_back( 0 );
 			}
 		}
 	}
+	// 사용되었던 버퍼를 비워준다.
 	d3d::Release<ID3DXBuffer*>(mtrlBuffer); // done w/ buffer
 
 	//
 	// Optimize the mesh.
 	//
 
-	hr = Mesh->OptimizeInplace(		
+	// X파일에서 받아온 Mesh에서 최적화시킨다.
+	hr = Mesh->OptimizeInplace(	
+		// 속성으로 삼각형을 정렬하고 속성테이블을 생성
 		D3DXMESHOPT_ATTRSORT |
+		// 사용되지 않는 인덱스와 버텍스 제거
 		D3DXMESHOPT_COMPACT  |
+		// 버텍스 캐시의 히트율 높임
 		D3DXMESHOPT_VERTEXCACHE,
 		(DWORD*)adjBuffer->GetBufferPointer(),
 		0, 0, 0);
 
+	// 사용완료한 인접버퍼 릴리즈
 	d3d::Release<ID3DXBuffer*>(adjBuffer); // done w/ buffer
 
 	if(FAILED(hr))
@@ -206,6 +233,7 @@ bool Display(float timeDelta)
 
 		for(int i = 0; i < Mtrls.size(); i++)
 		{
+			// 각 서브셋에 맞는 재질과, 텍스처를 적용하고 그 서브셋에 포함되는 삼각형들을 그린다.
 			Device->SetMaterial( &Mtrls[i] );
 			Device->SetTexture(0, Textures[i]);
 			Mesh->DrawSubset(i);
